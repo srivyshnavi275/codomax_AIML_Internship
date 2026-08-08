de667c7436340fcc9fae9f2409f5fb6b113fd276
Vyshnavi Sri <vyshnavisri275@gmail.com>
2026-08-08 12:32:32 +0530

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,51 @@
+#Variables 
+name = "SAM"
+age = 20
+height = 5.6
+
+#printing the variables
+print(name)
+print(age)
+print(height)
+
+#DATA TYPES
+print(type(name))
+print(type(age))
+print(type(height))
+
+#Operators
+#Arithmetic operators
+a=10
+b=5
+print(a + b)
+print(a - b)
+print(a  * b)
+print(a / b)
+print(a % b)
+print(a // b)
+
+#Comparison operators
+print(a > b)
+print(a < b)
+print(a == b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+#Loops
+#For loop
+for i in range(1,5):
+    print(i)
+
+#whole loop
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+ #Functions
+    def greet (name):
+        print("Hello",name)
+    greet("SAM")
+    greet("john")
+     
+
